5c45385c2702108f3b2a3961d299df8ce98cbd99
Otabek Samatov <otabeksamatov11235@gmail.com>
2020-04-29 18:45:44 -0500

    backend init

--- state_class/provider.tf
+++ state_class/provider.tf
@@ -3,8 +3,16 @@ provider "aws" {
   version = "2.59"
 }
 
-terraform {
-  required_version= "0.11.14"
+resource "aws_key_pair" "us-east-1-key" {
+  key_name   = "state_bastion"
+  public_key = "${file("~/.ssh/id_rsa.pub")}"
 }
 
-
+terraform {
+  required_version= "0.11.14"
+  backend "s3" {
+    bucket = "mybucket"
+    key    = "path/to/my/key"
+    region = "us-east-1"
+  }
+}
